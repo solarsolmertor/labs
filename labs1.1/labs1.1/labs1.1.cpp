488f97a53ccdbc7ae902362a0590b70717d9cd8c
solarsolmertor <Karyna.Shves@cs.khpi.edu.ua>
2021-12-20 21:47:39 +0200

    lab1 by Karina Shvets

---
+++ labs1.1/labs1.1/labs1.1.cpp
@@ -0,0 +1,9 @@
+int main() {
+    int integerValue = sizeof(int); // integer in bytes
+    int shortValue = sizeof(short); // short in bytes
+    int longValue = sizeof(long); // long in bytes
+    int charValue = sizeof(char); // char in bytes
+    int floatValue = sizeof(float); // float in bytes
+    int doubleValue = sizeof(double); // double in bytes
+    return 0;
+}
---
+++ labs1.1/labs1.5/labs1.5.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    int userNumber = 5; // initial user value
+    int computerNumber = userNumber + rand(); // random unfair computer value
+    return 0;
+}
