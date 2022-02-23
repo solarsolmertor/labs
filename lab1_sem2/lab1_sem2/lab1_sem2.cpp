3f8feeecd5421bb5cbb6defb11d31a97cb619e25
solarsolmertor <91151667+solarsolmertor@users.noreply.github.com>
2022-02-23 13:01:29 +0200

    Add files via upload
    labs 1 by Karina Shvets (2 sem)

---
+++ lab1_sem2/lab1_sem2/lab1_sem2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void num(int number) {
+	if (number) {
+		num(number / 10);
+		cout << number % 10;
+	}
+}
+int rec(int number) {
+	if (number > 10) return 1 + rec(number / 10);
+	return 1;
+}
+int sum(int number) {
+	if (number / 10) return number % 10 + sum(number / 10);
+	else return number % 10;
+}
+int main() {
+	int number;
+	while (true) {
+		cout << "Enter natural number: ";
+		cin >> number;
+		if (number > 0) {
+			cout << "Recursive output of entered number: ";
+			num(number);
+			cout << endl;
+			cout << "Recursive output digits of number: " << rec(number) << endl;
+			cout << "Recursive output sum of digits: " << sum(number);
+			break;
+		}
+		else {
+			cout << "Wrong value! Please enter again: " << endl;
+		}
+	}
+	return 0;
+}
---
+++ lab1a_2sem/lab1a_2sem/lab1a_2sem.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int max_mas(int A[], int i, int max, int max2) {
+	if (i < 0) return max2;
+	if (A[i] > max) {
+		max2 = max;
+		max = A[i];
+	}
+	else if (A[i] > max2) {
+		max2 = A[i];
+	}
+	max_mas(A, i - 1, max, max2);
+}
+int main() {
+	const int n = 5;
+	int A[n];
+	cout << "Size of array: " << n << endl << "Enter values: ";
+	for (int i = 0; i < n; i++)
+		cin >> A[i];  
+	int max = INT_MIN;
+	int max2 = INT_MIN;
+	cout << "Second highest value in array: " << max_mas(A, n - 1, max, max2);
+	return 0;
+}
