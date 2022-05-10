a433004914f3e6a06ffe51d3b3a30cc4dc1a9e9b
solarsolmertor <91151667+solarsolmertor@users.noreply.github.com>
2022-05-10 19:07:43 +0300

    Labs 3 and 5 by Shvets Karina

---
+++ lab3_2sem/lab3_2sem/lab3_2sem.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    int size, sum = 0, amount = 0;
+    cout << "Enter size of array: ";
+    cin >> size;
+    int* arr = new int[size];
+    cout << "Initial values of array: ";
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 20 - 10;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int* arr1 = new int[amount + 1];
+    for (int i = 0; i < size; i++) {
+        if (abs(arr[i]) < 5) {
+            arr1[amount] = arr[i];
+            amount++;
+            sum += arr[i];
+        }
+    }
+    cout << "Total amount of absolute values < 5: " << amount << endl;
+    cout << "Sum of values: " << sum << endl;
+    cout << "Values of new array: ";
+    for (int i = 0; i < amount; i++) {
+        cout << arr1[i] << " ";
+    }
+    return 0;
+}
---
+++ lab5_2sem/lab5_2sem/lab5_2sem.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int size;
+    cout << "Enter size of matrix: ";
+    cin >> size;
+    int** mas = new int* [size];
+    for (int i = 0; i < size; i++)
+        mas[i] = new int[size];
+    int i, j, k = 1;
+
+    for (i = 0; i < size; i++)
+        for (j = 0; j <= i; j++)
+            mas[j][i - j] = k++;
+
+    for (i = 1; i < size; i++)
+        for (j = i; j < size; j++)
+            mas[j][size - j + i - 1] = k++;
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            cout << mas[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
